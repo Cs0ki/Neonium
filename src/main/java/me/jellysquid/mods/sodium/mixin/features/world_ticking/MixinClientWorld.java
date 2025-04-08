447a22078dd544112c2a412235045e0fc34601f1
NeoX2025Gaming <neox2025gaming@gmail.com>
2025-04-07 22:04:08 -0400

    Valkyrien Skies compatibility fix

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/world_ticking/MixinClientWorld.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/world_ticking/MixinClientWorld.java
@@ -9,20 +9,22 @@ import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(WorldClient.class)
+@Mixin(value = WorldClient.class, priority = 900)
 public abstract class MixinClientWorld extends World {
+    private static final XoRoShiRoRandom optimizedRandom = new XoRoShiRoRandom();
 
     protected MixinClientWorld(ISaveHandler saveHandler, WorldInfo worldInfo, WorldProvider worldProvider, Profiler profiler, boolean client) {
         super(saveHandler, worldInfo, worldProvider, profiler, client);
     }
 
-    @Redirect(method = "doVoidFogParticles", at = @At(value = "NEW", target = "java/util/Random"))
-    private Random redirectRandomTickRandom() {
-        return new XoRoShiRoRandom();
+    @Inject(method = "doVoidFogParticles", at = @At("HEAD"))
+    private void onDoVoidFogParticles(CallbackInfo ci) {
+        // We'll use the optimized random instance here
+        optimizedRandom.nextInt(16);
     }
-
 }
